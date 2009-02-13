1f339a3d1eee9f0d1fa8a3765ab04b23a742910b
Jarin Udom <jarin.udom@gmail.com>
2009-02-13 03:37:21 -0800

    Added basic Shopify API receiver

--- main.rb
+++ main.rb
@@ -2,11 +2,28 @@
 require 'rubygems'
 require 'sinatra'
 
+# Remove trailing slash from URIs reaching Sinatra
+before do
+  request.env['PATH_INFO'].gsub!(/\/$/, '') if request.env['PATH_INFO'] != '/'
+end
+
 ### CONTROLLER ACTIONS
 
 # index
 get '/' do
   haml :index
+end
+
+# Storekeeper page
+get '/storekeeper' do
+  "Storekeeper page"
+end
+
+# Storekeeper API key page
+get '/storekeeper/auth' do
+  @shop_url = params[:shop]
+  @auth_key = params[:t]
+  "Shop: #{@shop_url} - Key: #{@auth_key}"
 end
 
 # stylesheet
