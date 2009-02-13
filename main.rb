6597ed6c12659557a080aae796c88886157c4cf7
Jarin Udom <jarin.udom@gmail.com>
2009-02-13 15:57:01 -0800

    Changed Shopify API auth url to return the password

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 # main.rb
 require 'rubygems'
 require 'sinatra'
+require 'digest/md5'
 
 # Remove trailing slash from URIs reaching Sinatra
 before do
@@ -23,7 +24,7 @@ end
 get '/tools/storekeeper/auth' do
   @shop_url = params[:shop]
   @auth_key = params[:t]
-  "Shop: #{@shop_url} - Key: #{@auth_key}"
+  "Shop: #{@shop_url} - Password: #{Digest::MD5.hexdigest('1dc39ab1fb79c7f0a7295954929e0598' + @auth_key)}"
 end
 
 # stylesheet
