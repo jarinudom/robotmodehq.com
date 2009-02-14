675e7b5d8561fe1202c6c0079cd0b83c43d5203e
Jarin Udom <jarin.udom@gmail.com>
2009-02-13 17:29:27 -0800

    Changed storekeeper to shopkeeper

--- main.rb
+++ main.rb
@@ -16,12 +16,12 @@ get '/' do
 end
 
 # Storekeeper page
-get '/tools/storekeeper' do
+get '/tools/shopkeeper' do
   "Storekeeper page"
 end
 
 # Storekeeper API key page
-get '/tools/storekeeper/auth' do
+get '/tools/shopkeeper/auth' do
   @shop_url = params[:shop]
   @auth_key = params[:t]
   "Shop: #{@shop_url} - Password: #{Digest::MD5.hexdigest('1dc39ab1fb79c7f0a7295954929e0598' + @auth_key)}"
