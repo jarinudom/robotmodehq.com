cb71dadece600fd1f6401f73c3485879c389fa29
Jarin Udom <jarin.udom@gmail.com>
2009-02-13 17:38:58 -0800

    Updated Shopify secret key

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@ require 'rubygems'
 require 'sinatra'
 require 'digest/md5'
 
+
 # Remove trailing slash from URIs reaching Sinatra
 before do
   request.env['PATH_INFO'].gsub!(/\/$/, '') if request.env['PATH_INFO'] != '/'
@@ -24,7 +25,7 @@ end
 get '/tools/shopkeeper/auth' do
   @shop_url = params[:shop]
   @auth_key = params[:t]
-  "Shop: #{@shop_url} - Password: #{Digest::MD5.hexdigest('1dc39ab1fb79c7f0a7295954929e0598' + @auth_key)}"
+  "Shop: #{@shop_url} - Password: #{Digest::MD5.hexdigest('3f97c04ac6bc2fa30b5d4f0144fc58a7' + @auth_key)}"
 end
 
 # stylesheet
