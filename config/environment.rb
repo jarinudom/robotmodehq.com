6d7f50c3c23380c69b48ec980c0dc59e0a8199ab
Jarin Udom <jarin.udom@gmail.com>
2009-02-20 04:13:25 -0800

    Added Dash monitoring support

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,10 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "haml"
+  config.gem 'json'
+  config.gem 'fiveruns-dash-rails',
+             :lib => 'fiveruns_dash_rails',
+             :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
---
+++ config/initializers/dash.rb
@@ -0,0 +1,1 @@
+Fiveruns::Dash::Rails.start(:production => 'edce3af0ef8ad574f5a1966e5ee7c1b9b313a885') if defined?(Fiveruns::Dash)
