80b911d1150252c2a31ddacf1fd67fa933034247
Jarin Udom <jarin.udom@gmail.com>
2009-02-19 22:50:04 -0800

    Added restart.txt directive to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,3 +44,16 @@ desc "Make symlink for database yaml"
 task :symlink do
   run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
 end
+
+namespace :deploy do
+  
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
