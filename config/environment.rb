1044cd6b2227fa62dac26bb36ced29f8f845522a
Jarin Udom <jarin.udom@gmail.com>
2009-10-05 19:37:02 -0700

    Removed Dash

--- config/environment.rb
+++ config/environment.rb
@@ -21,9 +21,6 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "haml"
   config.gem 'json'
-  config.gem 'fiveruns-dash-rails',
-             :lib => 'fiveruns_dash_rails',
-             :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
--- config/initializers/dash.rb
+++
@@ -1,1 +0,0 @@
-Fiveruns::Dash::Rails.start(:production => 'edce3af0ef8ad574f5a1966e5ee7c1b9b313a885') if defined?(Fiveruns::Dash)
